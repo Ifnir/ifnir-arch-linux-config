e98a5ae96cf340fbd8339375f24e007276647ac8
Andreas <mrkingkake@gmail.com>
2022-01-14 09:45:08 +0100

    Update ifnir_setup.sh

--- ifnir_setup.sh
+++ ifnir_setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
+# how to put linux cmd into variable
 pw=$(pwd)
 
-appsname=(rofi i3 picom polybar)
+appsname=(rofi i3 picom polybar alacritty)
 
 for name in ${appsname[@]}; do    
     if [ ! -d ~/.config/$name ]; then
@@ -16,7 +18,7 @@ fi;
 
 if [ ! -f ~/.config/i3/config ];
 then
-    ln -s $pw/i3/config ~/.config/i3/config
+    ln -s $pw/i3/config.conf ~/.config/i3/config
 fi;
 
 if [ ! -f ~/.config/alacritty/alacritty.yml ];
@@ -24,9 +26,13 @@ then
     ln -s $pw/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 fi;
 
-if [ ! -f ~/.config/polybar/config ] && [ ! -f ~/.config/polybar/launch.sh];
+if [ ! -f ~/.config/polybar/config ];
 then
-    ln -s $pw/polybar/config ~/.config/polybar/config
-    ln -s $pw/polybar/launch.sh ~/.config/polubar/launch.sh
+    ln -s $pw/polybar/config.conf ~/.config/polybar/config
+    
 fi;
 
+if [ ! -f ~/.config/polybar/launch.sh ];
+then
+    ln -s $pw/polybar/launch.sh ~/.config/polybar/launch.sh
+fi;
