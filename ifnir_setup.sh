274ad99bcda0083e5e4eaaddd8740590fdf592b6
Andreas <mrkingkake@gmail.com>
2022-01-14 09:10:31 +0100

    Update ifnir_setup.sh

--- ifnir_setup.sh
+++ ifnir_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+pw=$(pwd)
 
 appsname=(rofi i3 picom polybar)
 
@@ -10,6 +11,22 @@ done;
 
 if [ ! -f ~/.config/rofi/config.rasi ];
 then
-    ln -s $(pwd)/rofi/config.rasi ~/.config/rofi/config.rasi
+    ln -s $pw/rofi/config.rasi ~/.config/rofi/config.rasi
+fi;
+
+if [ ! -f ~/.config/i3/config ];
+then
+    ln -s $pw/i3/config ~/.config/i3/config
+fi;
+
+if [ ! -f ~/.config/alacritty/alacritty.yml ];
+then
+    ln -s $pw/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+fi;
+
+if [ ! -f ~/.config/polybar/config ] && [ ! -f ~/.config/polybar/launch.sh];
+then
+    ln -s $pw/polybar/config ~/.config/polybar/config
+    ln -s $pw/polybar/launch.sh ~/.config/polubar/launch.sh
 fi;
 
