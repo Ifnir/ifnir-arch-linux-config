7082aa3bbb4eb572ffb04d9594395ddba7424971
Andreas <aka@jma.dk>
2022-04-05 11:20:01 +0200

    update picom polybar i3

--- picom/launch.sh
+++ picom/launch.sh
@@ -2,6 +2,6 @@
 
 killall -q picom
 
-#while grep -u $UID -x picom >/dev/null; do sleep 1; done
+while grep -u $UID -x picom >/dev/null; do sleep 1; done
 
 picom -bcCGf --active-opacity 0.9 -i 0.7 -e 0.8 --no-fading-openclose --opacity-rule "100:name *= 'Firefox'" --opacity-rule "100:name *= 'rofi'" --opacity-rule "100:name *= 'Google Chrome'" 
